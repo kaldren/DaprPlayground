bf46cb1a536208a143a116bba282dd6b008c66a3
Kaloyan <Kaloyan@DESKTOP-B1HG11K>
2024-09-11 10:41:42 +0300

    Add project files.

---
+++ StateStore/Program.cs
@@ -0,0 +1,17 @@
+﻿using Dapr.Client;
+
+const string DAPR_STORE_NAME = "statestore";
+const string KEY = "last-check";
+
+while (true)
+{
+    Thread.Sleep(3000);
+    using var client = new DaprClientBuilder().Build();
+
+    await client.SaveStateAsync(DAPR_STORE_NAME, KEY, DateTimeOffset.UtcNow);
+
+    Console.WriteLine("State saved successfully");
+
+    var getStateResponse = await client.GetStateAsync<string>(DAPR_STORE_NAME, KEY);
+    Console.WriteLine($"State retrieved successfully. Retrieved value: {getStateResponse}");
+}
