7d54d48c4cf8d22f1fbbf4c8b7f19d3ae6a2705f
kaldren <drenski666@hotmail.com>
2024-09-11 15:06:15 +0300

    http request

---
+++ FrontendApp/Pages/Weather.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Net.Http.Json;
+
+namespace FrontendApp.Pages;
+
+public class PrivacyModel : PageModel
+{
+    private readonly ILogger<PrivacyModel> _logger;
+    public List<WeatherForecast> WeatherForecasts { get; private set; } = new();
+
+    public PrivacyModel(ILogger<PrivacyModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public async Task OnGet()
+    {
+
+
+        try
+        {
+            using var httpClient = new HttpClient();
+            WeatherForecasts = await httpClient.GetFromJsonAsync<List<WeatherForecast>>("http://localhost:5250/weatherforecast") ?? new();
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error fetching weather forecast");
+        }
+    }
+
+    public record WeatherForecast(DateOnly Date, int TemperatureC, int TemperatureF, string? Summary);
+}
+
